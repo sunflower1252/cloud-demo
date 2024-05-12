b8f6cf4639647bb0ed2f8e40a1c3b0e7aea498a5
yg <3141219704@qq.com>
2024-05-12 13:35:45 +0800

    实现eureka的启动

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args) {
+        System.out.println("-----------EurekaServerApplication-----------");
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
