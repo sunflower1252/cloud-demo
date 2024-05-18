629fbb40766c716426f8b66bf0876b8609f7a473
31412 <3141219704@qq.com>
2024-05-18 17:02:23 +0800

    修改了包含小错误的pom文件

--- mq-server/src/main/java/com/example/mqserver/MqServerApplication.java
+++ mq-server/src/main/java/com/example/mqserver/MqServerApplication.java
@@ -2,11 +2,13 @@ package com.example.mqserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author YG
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class MqServerApplication {
 
     public static void main(String[] args) {
--- mq-server/src/test/java/com/example/mqserver/MqServerApplicationTests.java
+++ mq-server/src/test/java/com/example/mqserver/MqServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.mqserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
 class MqServerApplicationTests {
